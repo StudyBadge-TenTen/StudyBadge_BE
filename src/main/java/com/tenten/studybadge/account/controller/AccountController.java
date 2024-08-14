b118d49336f6e9d4ddae1651147c6cd50d3bdca7
Ahn <youn_tip@naver.com>
2024-08-14 19:58:48 +0900

    style: STD-47 맵핑 수정

--- src/main/java/com/tenten/studybadge/account/controller/AccountController.java
+++ src/main/java/com/tenten/studybadge/account/controller/AccountController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +19,7 @@ public class AccountController {
     @Operation(summary = "계좌 인증", description = "회원정보 수정 시 계좌인증(예금주조회)")
     @Parameter(name = "BankCode", description = "은행코드")
     @Parameter(name = "BankNum", description = "계좌번호")
-    @GetMapping("/api/cert/account")
+    @PostMapping("/api/cert/account")
     public ResponseEntity<Void> certAccount(@LoginUser Long memberId,
                                             @RequestBody AccountRequest accountRequest) {
 
